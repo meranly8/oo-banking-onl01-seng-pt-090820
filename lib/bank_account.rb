ccd7a3cdcd9ff00e56dab13a5206fdc1056e434c
Michelle Ranly <meranly8@gmail.com>
2020-10-06 04:09:43 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,7 +17,7 @@ class BankAccount
   end
   
   def valid?
-    self.status == "open" && self.balance > 0 ? true : false
+    self.status == "open" && self.balance > 0
   end
   
   def close_account
